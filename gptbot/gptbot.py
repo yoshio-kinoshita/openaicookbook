f6310e353bf1ed72884dba5b5115d245d93d47ad
協力会社 GRAPE 木下 <grape.kinoshita@sg.itcc.co.jp>
2023-11-02 17:10:59 +0900

    add gpt bot

--- gptbot/gptbot.py
+++ gptbot/gptbot.py
@@ -2,39 +2,59 @@ from flask import Flask, request, jsonify, render_template
 import openai
 import os
 import json
+import markdown
+import logging
 
-openai.api_key = os.environ.get('OPENAI_API_KEY')
+openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 app = Flask(__name__)
 
-@app.route('/')
+
+# ファイルへのハンドラを作成します。
+file_handler = logging.FileHandler("app.log")
+
+# ハンドラにフォーマッタを設定します。
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+file_handler.setFormatter(formatter)
+
+# ハンドラをロガーに追加します。
+app.logger.addHandler(file_handler)
+
+
+@app.route("/")
 def hello_world():
-    return render_template('client.html')
+    return render_template("client.html")
+
 
 @app.route("/api/chat", methods=["POST"])
 def chat():
-    data = request.get_data().decode('utf8')
+    data = request.get_data().decode("utf8")
     data = json.loads(data)
     message = data["message"]
 
-    # response = openai.Completion.create(
-    #     engine="text-davinci-002",
-    #     prompt=message,
-    #     temperature=0.5,
-    #     max_tokens=60
-    # )
+    app.logger.info(message)
 
     response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "system", "content": "あなたは優秀なプログラマで、質問に対してとてもわかりやすい回答ができます。正確な回答を得るため、不明瞭なところは質問者に確認してください。"},
-        {"role": "user", "content": message},
-    ]
+        model="gpt-3.5-turbo",
+        max_tokens=1000,
+        messages=[
+            {
+                "role": "system",
+                "content": "あなたは優秀なプログラマで、質問に対してとてもわかりやすい回答ができます。正確な回答を得るため、不明瞭なところは質問者に確認してください。",
+            },
+            {"role": "user", "content": message},
+        ],
     )
 
-    # chatbot_response = response.choices[0].text.strip()
-    chatbot_response = response['choices'][0]['message']['content']
-    return jsonify({"response": chatbot_response})
+    chatbot_response = response["choices"][0]["message"]["content"]
+    md = markdown.markdown(chatbot_response)
+
+    app.logger.info(chatbot_response)
+
+    return jsonify({"response": md})
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.logger.setLevel(logging.DEBUG)
+    os.environ["FLASK_ENV"] = "development"
+    app.run(host="0.0.0.0", port=5000, debug=True)
---
+++ gptbot/static/js/common.js
@@ -0,0 +1,60 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var myForm = document.getElementById('myForm');
+    myForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+        var msg = document.getElementById('message').value;
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', '/api/chat');
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.onload = function () {
+            if (xhr.status === 200) {
+                var response = JSON.parse(xhr.responseText).response;
+                document.getElementById('response').innerHTML = response;
+            } else {
+                alert('request failed ' + xhr.statusText);
+            }
+        };
+        xhr.send(JSON.stringify({ message: msg }));
+    });
+});
+
+const chatLog = document.getElementById('chat-log');
+const userInput = document.getElementById('user-input');
+const submitBtn = document.getElementById('submit-btn');
+
+submitBtn.addEventListener('click', () => {
+    const userMessage = userInput.value;
+    displayUserMessage(userMessage);
+    sendToBot(userMessage);
+    userInput.value = '';
+});
+
+function displayUserMessage(message) {
+    const userDiv = document.createElement('div');
+    userDiv.classList.add('user-message');
+    userDiv.textContent = message;
+    chatLog.appendChild(userDiv);
+}
+
+function displayBotMessage(message) {
+    const botDiv = document.createElement('div');
+    botDiv.classList.add('bot-message');
+    botDiv.innerHTML = message;
+    chatLog.appendChild(botDiv);
+}
+
+function sendToBot(message) {
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', '/api/chat');
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            var response = JSON.parse(xhr.responseText).response;
+            displayBotMessage(response);
+        } else {
+            alert('request failed ' + xhr.statusText);
+        }
+    };
+    xhr.send(JSON.stringify({ message: message }));
+}
